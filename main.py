40bb870e18516f9b15263ed5f7e2bbb347d2d20b
Samuel Mitchell <100097033+sammit52@users.noreply.github.com>
2023-03-09 05:42:32 +0000

    Added random quote command Tells user if there is no quotes in database Used secrets to fix the token Fixed errors, see test sheet

--- main.py
+++ main.py
@@ -1,40 +1,48 @@
+import os
 import sqlite3
 import discord
 import random
 from discord.ext import commands
 
-TOKEN = "MTA4MDk2MDY2MTY2NTg3NDA2MA.GtTPoT.IWvile2TFdYZKkL6r4ZMjvBEPKTtz7E1XgKM-A"
+TOKEN = os.environ["TOKEN"]
 
-# Connect to the database
-conn = sqlite3.connect('quotes.db')
 
-# Create a cursor object
-cursor = conn.cursor()
+intents = discord.Intents.default()
+intents.message_content = True
+client = commands.Bot(command_prefix="!", intents=intents)
 
-# Create a table to store the quotes
-cursor.execute('CREATE TABLE IF NOT EXISTS quotes (id INTEGER PRIMARY KEY, quote TEXT)')
 
-# Prompt the user to enter a quote
-quote = input('Enter a quote: ')
+@client.command(name="randomquote")
+async def random_quote(ctx):
+  # Connect to the database
+  conn = sqlite3.connect('quotes.db')
 
-# Insert the quote into the database
-cursor.execute('INSERT INTO quotes (quote) VALUES (?)', (quote,))
+  # Create a cursor object
+  cursor = conn.cursor()
 
-# Commit changes
-conn.commit()
+  # Create a table to store the quotes if one does not exist already
+  cursor.execute(
+    'CREATE TABLE IF NOT EXISTS quotes (id INTEGER PRIMARY KEY, quote TEXT)')
 
-# Select all quotes from the database
-cursor.execute('SELECT quote FROM quotes')
+  # Select all quotes from the database
+  cursor.execute('SELECT quote FROM quotes')
 
-# Fetch all quotes
-rows = cursor.fetchall()
+  # Fetch all quotes
+  rows = cursor.fetchall()
 
-# Print all quotes
-print('All quotes in the database:')
-for row in rows:
-    print(row[0])
+  # Initialises quote as a variable that says there is no quotes
+  quote = "There are no quotes in the database. Use !addquote followed by your quote to add a quote."
 
-# Close the database connection
-conn.close()
+  # Checks whether the are quotes in the database, and if so randomly assigns one to the quote varriable
+  if rows:
+    i = random.randint(0, len(rows) - 1)
+    quote = rows[i][0]
+  
+  # Close the database connection
+  conn.close()
 
-print('Quote added successfully!')
+  # Send the quote to the user where row[i][0] is the i value of the last element
+  await ctx.channel.send(quote)
+
+@client.command()
+client.run(TOKEN)
